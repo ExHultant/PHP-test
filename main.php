b02d45ab58035028a8d2de0f4d60da6356ba1c87
Ricardo Chavez <74266998+ExHultant@users.noreply.github.com>
2023-01-12 17:25:04 -0400

    Add files via upload

---
+++ main.php
@@ -0,0 +1,6 @@
+<?php print_r($_REQUEST); 
+
+$a = array ('a' => 'manzana', 'b' => 'banana', 'c' => array ('x', 'y', 'z'));
+print_r ($a);
+
+?>
---
+++ operators-math.php
@@ -0,0 +1,5 @@
+<?php echo max(10, 30, 2, 300, 6)."<br>";
+echo min(1, 10, -3, 20)."<br>";
+echo pi()."<br>";
+echo rand()."<br>";
+echo sqrt(20); ?>
